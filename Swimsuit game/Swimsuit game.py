9362c3906b5f24d393f017ca311d18c296422585
Andy <33329682+aeom0987@users.noreply.github.com>
2019-04-23 19:10:28 -0400

    Added code per Arcade tutorial
    1)Added Header and body comments
    2)Added code following the arcade tutorial located here:
    http://arcade.academy/examples/platform_tutorial/index.html#open-a-window
    3) Added Prize_list to sprite lists
    4) Added sounds folders
    5) Added images folder
    6) Added sounds folder - files from tutorial
    7) moved image files to image folder and revised code to new path
    8) Added physics engine
    9) Added gravity
    10) Added ground and crates
    11) Added jumping

--- Swimsuit game/Swimsuit game.py
+++ Swimsuit game/Swimsuit game.py
@@ -1,23 +1,62 @@
+"""
+BEACH BUM BOB GOES TO THE BEACH (or other title)
+
+SDEV 140 Spring 2019 - Final Project
+Version 1.0
+
+Authors:
+Andy Olson Moyano
+Parker Staph
+Isaac Davis
+
+Credits:
+Tiles: created using tiled.com
+
+
+"""
 import arcade
 import os
 
-SPRITE_SCALING_PLAYER = 0.3
+######################## CONSTANTS ####################
+# Screen
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-MOVEMENT_SPEED = 3
+SCREEN_TITLE = "Beach Bum Bob Goes To The Beach"
 
+# Sprite scaling
+SPRITE_SCALING_PLAYER = 0.3
+TILE_SCALING = 0.5
+PRIZE_SCALING = 0.5
+
+# Physics and movement
+MOVEMENT_SPEED = 3
+GRAVITY = 1
+PLAYER_JUMP_SPEED = 15
+
+# How many pixels to keep as a minimum margin between the character
+# and the edge of the screen.
+LEFT_VIEWPORT_MARGIN = 150
+RIGHT_VIEWPORT_MARGIN = 150
+BOTTOM_VIEWPORT_MARGIN = 100
+TOP_VIEWPORT_MARGIN = 100
 
 class MyGame(arcade.Window):
 
     def __init__(self):
-        
+        """
+        Initializer
+        """
+
+        # Call the parent class initializer        
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, " ")
 
-        #Lists
+        #Variables that will hold sprite lists
         self.player_list = None
+        self.wall_list = None
+        self.prize_list = None
         self.background = None
 
-        #Sptite of player
+        #Sprite of player
         self.player_sprite = None
 
         #No cursor
@@ -25,39 +64,80 @@ class MyGame(arcade.Window):
 
         #Background
         arcade.set_background_color(arcade.color.AMAZON)
-        self.background = arcade.load_texture("Beach background.png")
+        self.background = arcade.load_texture("images/backgrounds/Beach background.png")
 
     def setup(self):
-        #sets up game and initialzes
+        #sets up game and initializes variables
 
+        #Sprite Lists
         self.player_list = arcade.SpriteList()
+        self.prize_list = arcade.SpriteList()
+        self.wall_list = arcade.SpriteList()
 
-        self.player_sprite = arcade.Sprite("Swimsuit Guy.png", SPRITE_SCALING_PLAYER)
+        #Setup player
+        self.player_sprite = arcade.Sprite("images/player_1/Swimsuit Guy.png", SPRITE_SCALING_PLAYER)
         self.player_sprite.center_x = 60
         self.player_sprite.center_y = 150
         self.player_list.append(self.player_sprite)
 
+        # Create the ground
+        # This shows using a loop to place multiple sprites horizontally
+        for x in range(0, 1250, 64):
+            wall = arcade.Sprite("images/tiles/tutorial/grassMid.png", TILE_SCALING)
+            wall.center_x = x
+            wall.center_y = 32
+            self.wall_list.append(wall)
+
+        # Put some crates on the ground
+        # This shows using a coordinate list to place sprites
+        coordinate_list = [[512, 96],
+                           [256, 96],
+                           [768, 96]]
+
+        for coordinate in coordinate_list:
+            # Add a crate on the ground
+            wall = arcade.Sprite("images/tiles/tutorial/boxCrate_double.png", TILE_SCALING)
+            wall.position = coordinate
+            self.wall_list.append(wall)
+
+        # Create the 'physics engine'
+        self.physics_engine = arcade.PhysicsEnginePlatformer(self.player_sprite, self.wall_list, GRAVITY)
+        
     def on_draw(self):
+        # Renders the screen
         arcade.start_render()
+        
         arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2,
                                       SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
-        self.player_list.draw()
 
+        # Draw all the sprites
+        self.player_list.draw()
+        self.wall_list.draw()
+        self.prize_list.draw()
+        
     def update(self, delta_time):
-        self.player_list.update()
+        """ Movement and game logic """
+
+        # Call update to move the sprite
+        # If using a physics engine, call update on it instead of the sprite
+        # list.        
+        self.physics_engine.update()
 
     def on_key_press(self, key, modifiers):
+        """Called whenever a key is pressed. """
+
         if key == arcade.key.UP:
-            self.player_sprite.change_y = MOVEMENT_SPEED
+            if self.physics_engine.can_jump():
+                self.player_sprite.change_y = PLAYER_JUMP_SPEED         
         elif key == arcade.key.LEFT:
             self.player_sprite.change_x = -MOVEMENT_SPEED
         elif key == arcade.key.RIGHT:
             self.player_sprite.change_x = MOVEMENT_SPEED
 
     def on_key_release(self, key, modifiers):
-        if key == arcade.key.UP or key == arcade.key.DOWN:
-            self.player_sprite.change_y = 0
-        elif key == arcade.key.LEFT or key == arcade.key.RIGHT:
+        """Called when the user releases a key. """
+
+        if key == arcade.key.LEFT or key == arcade.key.RIGHT:
             self.player_sprite.change_x = 0
 
 def main():
